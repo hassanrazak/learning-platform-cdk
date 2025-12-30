a85baaacc0961a898953380d2cb7c607c7fd60ed
hassanrazak <76017055+hassanrazak@users.noreply.github.com>
2025-12-30 15:01:29 -0500

    added comments to specify front end and backend repo

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -10,10 +10,11 @@ export const MEDIA_CONVERT_SERVICE = 'mediaconvert.amazonaws.com';
 export const LAMBDA_SERVICE = 'lambda.amazonaws.com';
 export const EC2_SERVICE = 'ec2.amazonaws.com';
 export const STS_SERVICE = 'sts.amazonaws.com';
-// "hassan/learning-platform"
+// "hassan/learning-platform backend"
 export const LP_SERVICE_REPO = 'hassan/learning-platform';
+// "hassan/learning-platform frontend"
 export const LP_FRONTEND_REPO = 'hassan/learning-platform-frontend';
-// "hassan/learning-platform-cdk"
+// "hassan/learning-platform-cdk "
 export const LP_CDK_REPO = 'hassan/learning-platform-cdk';
 export const GITHUB_OIDC_TOKEN_URL = 'https://token.actions.githubusercontent.com';
 export const BRANCH = {
